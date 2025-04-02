bd1311696fda881b1048dbea17b6937ac8f932e9
Abderrahmane TAHRI JOUTI <atahrijouti@paypal.com>
2025-04-02 17:02:05 +0200

    better struct I think

---
+++ lib/client/index.ts
@@ -0,0 +1,4 @@
+export type { Metadata, Module } from "../types"
+export { textToHTML } from "./html"
+export { $loop } from "./render"
+export { html, css } from "./tags"
--- lib/index.ts
+++
@@ -1,4 +0,0 @@
-export type { Metadata, Module } from "./types"
-export { textToHTML } from "./utils/html"
-export { $loop } from "./utils/render"
-export { html, css } from "./utils/tags"
